ee945bd0931d7dc7b59fb7bef769a118f23e2cd8
hsdarhan <hsdarhan@edu.uwaterloo.ca>
2021-01-19 00:19:44 -0500

    Prediction module

---
+++ src/backend/predict.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+from tensorflow.keras.preprocessing import image
+import numpy as np
+
+def predict(picture):
+    loaded_model = tf.keras.models.load_model('./saved/model.h5')
+    picture = image.load_img(picture, target_size = (299,299))
+    picture = image.img_to_array(picture)
+    picture = np.expand_dims(picture, axis=0)
+    picture /= 255.
+
+    prediction = loaded_model.predict(picture)
+    index = np.argmax(prediction)
+
+    food_classes = ['pancackes', 'donuts', 'pizza', 'pho', 'hamburger']
+    food_classes.sort()
+    
+    return food_classes[index]
